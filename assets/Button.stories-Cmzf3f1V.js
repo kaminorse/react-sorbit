76d9eb2d986917bf09de340bf7f41366c8ab3e4a
kaminorse <kaminorse@users.noreply.github.com>
2025-02-20 03:34:15 +0000

    deploy: 0636b118e58e11af10e8f4c80abb5312c6ebf5ba

---
+++ assets/Button.stories-Cmzf3f1V.js
@@ -0,0 +1,6 @@
+import{j as r}from"./jsx-runtime-CkxqCPlQ.js";import{B as e}from"./Button-CQm0ZrKy.js";import{S as p}from"./SorbitStatic-CD4P5ev_.js";import"./index-DJO9vBfz.js";import"./Soroot-DP67-vXi.js";const S={title:"Components/Button",component:e,tags:["autodocs"],argTypes:{}},t={args:{},render:n=>r.jsx(p,{children:r.jsx(e,{...n})})};var o,s,a;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {},
+  render: props => <SorbitStatic>
+      <Button {...props} />
+    </SorbitStatic>
+}`,...(a=(s=t.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const l=["Plain"];export{t as Plain,l as __namedExportsOrder,S as default};
